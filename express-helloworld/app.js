85a356fb612c7436aae736c829cb9b869447cecf
franciscoBlanco2 <131667045+franciscoBlanco2@users.noreply.github.com>
2023-04-26 09:47:34 +0200

    Update app.js
    cambios debido a updates

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World! FBG\n');
+  res.send('Hello World! FBG Miercoles\n');
 });
 app.get('/Alex', function(req, res) {
   res.send('Hello!\n');
